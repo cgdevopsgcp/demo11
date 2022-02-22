7af5c7a1d3d43cc00c1f3ed19d2ac713a22607fe
cgdevopsgcp <97515980+cgdevopsgcp@users.noreply.github.com>
2022-02-22 14:34:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ access_config {
 }
 
 resource "google_storage_bucket" "auto-expire" {
-name = "buckett132"
+name = "buckett135"
 location = "US"
 force_destroy = true
 lifecycle_rule {
@@ -44,20 +44,5 @@ resource "google_compute_network" "vpc_network" {
   mtu                     = 1460
 }
 
-resource "google_compute_instance" "test12" {
-count = 1 // Adjust as desired
-name = "test12${count.index + 1}" // yields "test1", "test2", etc. It's also
-machine_type = "f1-micro" // smallest (CPU & RAM) available instance
-zone = "${var.region}" // yields "europe-west1-d" as setup previously.
-boot_disk {
-initialize_params {
-image = "debian-7-wheezy-v20160301" 
-}
-}
-network_interface {
-network = "default"
-access_config {
-}
-}
-}
+
 
