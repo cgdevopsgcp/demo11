e93b883924f3d4b7da948d869ebbc3258c825c5a
cgdevopsgcp <97515980+cgdevopsgcp@users.noreply.github.com>
2022-02-22 14:43:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ type = "Delete"
 
 resource "google_compute_network" "vpc_network" {
 //  project                 = "cg-anz-practice" # Replace this with your project ID in quotes
-  name                    = "my-auto-mode-network"
+  name                    = "my-auto-node-network"
   auto_create_subnetworks = true
   mtu                     = 1460
 }
